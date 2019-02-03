75118e4b5633d756a60a51957374c922255b0644
Rory Smith <rory.smith@korelogic.co.uk>
2019-02-03 17:28:36 +0000

    Initial commit proper

---
+++ src/01/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Spring } from 'react-spring';
+import { easeBounceOut } from 'd3-ease';
+import './styles.css';
+
+const circlePath = `M18 2.0845
+a 15.9155 15.9155 0 0 1 0 31.831
+a 15.9155 15.9155 0 0 1 0 -31.831`;
+
+const svgConfig = { easing: easeBounceOut, duration: 2500 };
+
+const Wheel = ({ stroke }) => (
+  <svg viewBox="0 0 36 36" class="wheel" width="100">
+    <path className="circle-bg" d={circlePath} />
+    <path
+      className="circle"
+      strokeDasharray={`${stroke}, 100`}
+      d={circlePath}
+    />
+    <text x="18" y="20.35" class="percentage">
+      {Math.round(stroke)}%
+    </text>
+  </svg>
+);
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Pace</h2>
+      <Spring
+        from={{ stroke: 0 }}
+        to={{ stroke: 54 }}
+        config={svgConfig}
+      >
+        {style => <Wheel stroke={style.stroke} />}
+      </Spring>
+      <h2>Stamina</h2>
+      <Spring
+        from={{ stroke: 0 }}
+        to={{ stroke: 30 }}
+        config={svgConfig}
+      >
+        {style => <Wheel stroke={style.stroke} />}
+      </Spring>
+      <h2>Shooting</h2>
+      <Spring
+        from={{ stroke: 0 }}
+        to={{ stroke: 90 }}
+        config={svgConfig}
+      >
+        {style => <Wheel stroke={style.stroke} />}
+      </Spring>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/02/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactPlaceholder from 'react-placeholder';
+import 'react-placeholder/lib/reactPlaceholder.css';
+import { Trail, config } from 'react-spring';
+import times from 'lodash.times';
+import './styles.css';
+
+const items = times(5, i => ({
+  key: i,
+  component: <ReactPlaceholder type="media" rows={2} />
+}));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <Trail
+            items={items}
+            keys={item => item.key}
+            from={{ opacity: 0, transform: 'translateY(-50%)' }}
+            to={{ opacity: 1, transform: 'translateY(0%)' }}
+            config={config.wobbly}
+          >
+            {item => props => <li style={props}>{item.component}</li>}
+          </Trail>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
